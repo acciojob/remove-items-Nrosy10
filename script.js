df7596d8f0b037bfd994e5131c7a2bdd2761a916
Nrosy10 <117885685+Nrosy10@users.noreply.github.com>
2023-06-10 10:14:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+	let list= document.getElementById("colorSelect");
+	let removeBtn= document.getElementsByTagName("input")[0];
+	removeBtn.addEventListener("click", function(){
+		let opt=list.options[list.selectedIndex];
+		list.removeChild(opt);
+	});
+
+// const colorSelect = document.getElementById("colorSelect");
+//     const removeColorButton = document.getElementById("removeColorButton");
+
+//     // Add a click event listener to the remove color button
+//     removeColorButton.addEventListener("click", function() {
+//       // Get the selected color option
+//       const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+
+//       // Remove the selected color option
+//       colorSelect.removeChild(selectedOption);
+//     });
